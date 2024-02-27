701d8fb3568068f5cc657e2671b45bbaaae195c2
artem skryhoniuk <fanat12341233@gmail.com>
2024-02-27 15:36:32 +0200

    init commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "ee67eb0ddbfbc4639ebb7a12c7546827"
 };
 
 /*
